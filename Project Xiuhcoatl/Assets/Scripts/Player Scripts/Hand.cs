cf31537e1843903e6ac62dda751fd544ede0c8dc
elucas431 <elucas2@asu.edu>
2018-07-31 21:02:23 -0700

    Torch Throw

--- Project Xiuhcoatl/Assets/Scripts/Player Scripts/Hand.cs
+++ Project Xiuhcoatl/Assets/Scripts/Player Scripts/Hand.cs
@@ -5,39 +5,74 @@ using UnityEngine;
 [RequireComponent(typeof(Controller))]
 public class Hand : MonoBehaviour
 {
-    GameObject heldObject;
+    GameObject heldObject, playerHead, reticle;
     Controller controller;
-    public Transform direction; 
     [HideInInspector]
     public bool isHeld; 
-
+    bool active; 
     Rigidbody simulator;
+    
 
+    private SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
     void Start()
     {
+        playerHead = GameObject.FindWithTag("Player Head");
+ 
         simulator = new GameObject().AddComponent<Rigidbody>();
         simulator.name = "simulator";
         simulator.transform.parent = transform.parent;
         controller = GetComponent<Controller>();
+        active = false;
     }
 
     void Update()
     {
+        playerHead.GetComponent<Head>().active = active;
         if (heldObject)
         {
-            simulator.velocity = (transform.position - simulator.position) * 50f;
-            simulator.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+            simulator.velocity = (transform.position - simulator.position) * 50;
+            simulator.angularVelocity = (transform.eulerAngles); //- simulator.transform.eulerAngles) * 2;
             if (controller.controller.GetPressUp(Valve.VR.EVRButtonId.k_EButton_Grip))
             {
                 isHeld = false; 
                 heldObject.transform.parent = null;
                 heldObject.GetComponent<Rigidbody>().isKinematic = false;
                 heldObject.GetComponent<Rigidbody>().useGravity = true;
-                heldObject.GetComponent<Rigidbody>().velocity = simulator.velocity;
-                heldObject.GetComponent<Rigidbody>().transform.eulerAngles = new Vector3(0, direction.transform.eulerAngles.y, 0);
+                heldObject.GetComponent<Rigidbody>().velocity = Controller.velocity * 1.5f;
+                heldObject.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
                 heldObject.GetComponent<HeldObject>().parent = null;
                 heldObject = null;
             }
+            if (controller.controller.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger))
+            {
+                active = true;
+                reticle = playerHead.GetComponent<Head>().temp;
+            }
+
+            playerHead.GetComponent<Head>().active = active;
+
+            if (controller.controller.GetPressUp(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger))
+            {
+
+                isHeld = false;
+                heldObject.transform.parent = null;
+                heldObject.GetComponent<Rigidbody>().isKinematic = false;
+                heldObject.GetComponent<Rigidbody>().useGravity = true;
+                heldObject.GetComponent<Rigidbody>().velocity = Controller.velocity * 3f;
+                heldObject.GetComponent<Rigidbody>().transform.LookAt(reticle.transform);
+                heldObject.GetComponent<HeldObject>().parent = null;
+                heldObject = null;
+                active = false; 
+            }
+
         }
         else
         {
@@ -59,37 +94,26 @@ public class Hand : MonoBehaviour
                     }
                 }
             }
-        }
-
-        if (controller.controller.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger))
-        { 
-            simulator.velocity = (transform.position - simulator.position) * 50f;
-        simulator.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
-        
-            Collider[] cols = Physics.OverlapSphere(transform.position, 0.1f);
-
-            foreach (Collider col in cols)
+            /*
+            if (controller.controller.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger))
             {
-                if (heldObject == null && col.GetComponent<HeldObject>() && col.GetComponent<HeldObject>().parent == null)
+                Collider[] cols = Physics.OverlapSphere(transform.position, 0.1f);
+
+                foreach (Collider col in cols)
                 {
-                    heldObject = col.gameObject;
-                    heldObject.transform.parent = transform;
-                    heldObject.transform.localPosition = Vector3.zero;
-                    heldObject.transform.localRotation = Quaternion.identity;
-                    heldObject.GetComponent<Rigidbody>().isKinematic = true;
-                    heldObject.GetComponent<HeldObject>().parent = controller;
+                    if (heldObject == null && col.GetComponent<HeldObject>() && col.GetComponent<HeldObject>().parent == null)
+                    {
+                        heldObject = col.gameObject;
+                        heldObject.transform.parent = transform;
+                        heldObject.transform.localPosition = Vector3.zero;
+                        heldObject.transform.localRotation = Quaternion.identity;
+                        heldObject.GetComponent<Rigidbody>().isKinematic = true;
+                        heldObject.GetComponent<HeldObject>().parent = controller;
+                    }
                 }
             }
+            */
         }
-    
-        else if (controller.controller.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger))
-        {
-            heldObject.transform.parent = null;
-            heldObject.GetComponent<Rigidbody>().isKinematic = false;
-            heldObject.GetComponent<Rigidbody>().velocity = simulator.velocity;
-            heldObject.GetComponent<HeldObject>().parent = null;
-            heldObject = null;
-            isHeld = false; 
-        }
+
     }
 }
---
+++ Project Xiuhcoatl/Assets/Scripts/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    float timer;
+    //float shootTime;
+    public float speed, interval;
+    bool spawnArrow;
+
+    void start()
+    {
+        timer = 0.0f;
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= interval)
+        {
+            transform.Translate(0, 0, Time.deltaTime * speed);
+        }
+
+        if (timer > 4.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}

f171eb9758d5370ffb1e9451b47477408d4b67c7
elucas431 <elucas2@asu.edu>
2018-08-03 22:32:45 -0700

    Death UI fix 1

---
+++ Project Xiuhcoatl/Assets/Scripts/Player Death/DeathSends/FallingDeath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingDeath : MonoBehaviour {
+
+    public GameObject playerCol;
+    GameObject player;
+
+
+    void Start () {
+        playerCol = GameObject.Find("CameraRig");
+        player = GameObject.Find("Player Head");
+    }
+	
+	void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            player.GetComponent<DeathCheck>().deathInput = 5;
+        }
+    }
+}
